c0c92605ac1941571e1044c063c112f07a7ef66a
Mukilan Thiyagarajan <mukilanthiagarajan@gmail.com>
2023-02-05 17:20:00 +0530

    Enforce code style with eslint

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 import { opendir, readFile, writeFile } from 'node:fs/promises'
 import { process_raw_results, score_run } from './process-wpt-results.js'
 
-async function readJsonFile(path) {
-  const contents = await readFile(path, {
-    encoding: 'utf8'
-  })
-  return JSON.parse(contents)
+async function read_json_file (path) {
+    const contents = await readFile(path, {
+        encoding: 'utf8'
+    })
+    return JSON.parse(contents)
 }
 
-async function writeJsonFile(path, json) {
-  const contents = JSON.stringify(json)
-  return writeFile(path, contents)
+async function write_json_file (path, json) {
+    const contents = JSON.stringify(json)
+    return writeFile(path, contents)
 }
 
-async function findLatestRun() {
-  const dir = await opendir('./runs') ;
-  const runs = []
-  for await (const run of dir) {
-    runs.push(run.name)
-  }
-
-  runs.sort()
-  console.log(runs)
-  return await readJsonFile(`./runs/${runs[runs.length - 1]}`)
-}
-async function main() {
-  const mode = process.argv[2];
-  if (!["--add", "--recalc"].includes(mode)) {
-    throw new Error(`invalid mode specified: ${mode}`)
-  }
-
-  let new_run
-  if (mode === "--add") {
-    const filename = process.argv[3]
-    const date = process.argv[4]
-    const matches = process.argv[5].match(/Servo ([0-9\.]+-[a-f0-9]+)?(-dirty)?$/)
-    let servo_version = "Unknown"
-    if (matches) {
-      servo_version = matches[1]
+async function find_latest_run () {
+    const dir = await opendir('./runs')
+    const runs = []
+    for await (const run of dir) {
+        runs.push(run.name)
     }
 
-    const results = await readJsonFile(filename)
-    new_run = process_raw_results(results)
-    new_run.run_info.browser_version = servo_version
-    await writeJsonFile(`./runs/${date}.json`, new_run)
-  } else if (mode === "--recalc") {
-    new_run = await findLatestRun()
-  }
+    runs.sort()
+    console.log(runs)
+    return await read_json_file(`./runs/${runs[runs.length - 1]}`)
+}
 
+async function main () {
+    const mode = process.argv[2]
+    if (!['--add', '--recalc'].includes(mode)) {
+        throw new Error(`invalid mode specified: ${mode}`)
+    }
 
-  const scores = [];
-  const dirs = await opendir('./runs') ;
-  for await (const dir of dirs) {
-    const [date, ] = dir.name.split('.')
-    const run = await readJsonFile(`./runs/${dir.name}`)
-    const score = score_run(run, new_run)
-    scores.push([
-      date,
-      score,
-      run.run_info.revision.substring(0, 6),
-      run.run_info.browser_version
-    ])
-  }
+    let new_run
+    if (mode === '--add') {
+        const filename = process.argv[3]
+        const date = process.argv[4]
+        const matches = process.argv[5].match(/Servo ([0-9.]+-[a-f0-9]+)?(-dirty)?$/)
+        let servo_version = 'Unknown'
+        if (matches) {
+            servo_version = matches[1]
+        }
 
-  writeJsonFile('./site/scores.json', { scores })
+        const results = await read_json_file(filename)
+        new_run = process_raw_results(results)
+        new_run.run_info.browser_version = servo_version
+        await write_json_file(`./runs/${date}.json`, new_run)
+    } else if (mode === '--recalc') {
+        new_run = await find_latest_run()
+    }
+
+    const scores = []
+    const dirs = await opendir('./runs')
+    for await (const dir of dirs) {
+        const [date] = dir.name.split('.')
+        const run = await read_json_file(`./runs/${dir.name}`)
+        const score = score_run(run, new_run)
+        scores.push([
+            date,
+            score,
+            run.run_info.revision.substring(0, 6),
+            run.run_info.browser_version
+        ])
+    }
+
+    write_json_file('./site/scores.json', { scores })
 }
 
 main()
--- test/process-wpt-results.test.js
+++ test/process-wpt-results.test.js
@@ -1,132 +1,134 @@
+/* eslint-env mocha */
+
 import { score_run, process_raw_results } from '../process-wpt-results.js'
 import { strict as assert } from 'assert'
 
 describe('Process wpt result', () => {
-  it('correctly transforms simple wpt results', () => {
-    const raw_result = {
-      run_info: {
-        product: "servo",
-        revision: "commitSha",
-        os: "Ubuntu"
-      },
-      results: [
-        {
-          test: "test1",
-          status: "PASS",
-          subtests: []
-        },
-        {
-          test: "test2",
-          status: "ERROR",
-          subtests: [
-            {
-              name: "subtest1",
-              status: "PASS"
+    it('correctly transforms simple wpt results', () => {
+        const raw_result = {
+            run_info: {
+                product: 'servo',
+                revision: 'commitSha',
+                os: 'Ubuntu'
             },
-            {
-              name: "subtest2",
-              status: "FAIL"
-            }
-          ]
+            results: [
+                {
+                    test: 'test1',
+                    status: 'PASS',
+                    subtests: []
+                },
+                {
+                    test: 'test2',
+                    status: 'ERROR',
+                    subtests: [
+                        {
+                            name: 'subtest1',
+                            status: 'PASS'
+                        },
+                        {
+                            name: 'subtest2',
+                            status: 'FAIL'
+                        }
+                    ]
+                }
+            ]
         }
-      ]
-    }
 
-    const processed = process_raw_results(raw_result)
-    assert.deepEqual(processed, {
-      run_info: raw_result.run_info,
-      test_scores: {
-        test1: {
-          score: 1,
-          subtests: {}
-        },
-        test2: {
-          score: 0,
-          subtests: {
-            subtest1: { score: 1 },
-            subtest2: { score: 0 }
-          }
-        }
-      }
+        const processed = process_raw_results(raw_result)
+        assert.deepEqual(processed, {
+            run_info: raw_result.run_info,
+            test_scores: {
+                test1: {
+                    score: 1,
+                    subtests: {}
+                },
+                test2: {
+                    score: 0,
+                    subtests: {
+                        subtest1: { score: 1 },
+                        subtest2: { score: 0 }
+                    }
+                }
+            }
+        })
     })
-  })
 })
 
 describe('Scoring', () => {
-  it('calculates scores for individual tests', () => {
-    const run = {
-      test_scores: {
-        test1: {
-          score: 1,
-          subtests: {}
-        },
-        test2: {
-          score: 1,
-          subtests: {}
+    it('calculates scores for individual tests', () => {
+        const run = {
+            test_scores: {
+                test1: {
+                    score: 1,
+                    subtests: {}
+                },
+                test2: {
+                    score: 1,
+                    subtests: {}
+                }
+            }
         }
-      }
-    }
 
-    let score = score_run(run, run)
-    assert.equal(score, 1000)
+        let score = score_run(run, run)
+        assert.equal(score, 1000)
 
-    run.test_scores.test2.score = 0
-    score = score_run(run, run)
-    assert.equal(score, 500)
-  })
-  it('calculates scores for subtests', () => {
-    const run = {
-      test_scores: {
-        test1: {
-          score: 0,
-          subtests: {
-            subtest1: { score: 1 },
-            subtest2: { score: 1 },
-            subtest3: { score: 1 },
-          }
+        run.test_scores.test2.score = 0
+        score = score_run(run, run)
+        assert.equal(score, 500)
+    })
+    it('calculates scores for subtests', () => {
+        const run = {
+            test_scores: {
+                test1: {
+                    score: 0,
+                    subtests: {
+                        subtest1: { score: 1 },
+                        subtest2: { score: 1 },
+                        subtest3: { score: 1 }
+                    }
+                }
+            }
         }
-      }
-    }
 
-    const score = score_run(run, run)
-    assert.equal(score, 1000)
-  })
-  it('calculates scores for subtests by averaging', () => {
-    const run = {
-      test_scores: {
-        test1: {
-          score: 0,
-          subtests: {
-            subtest1: { score: 1 },
-            subtest2: { score: 0 },
-            subtest3: { score: 0 },
-          }
+        const score = score_run(run, run)
+        assert.equal(score, 1000)
+    })
+    it('calculates scores for subtests by averaging', () => {
+        const run = {
+            test_scores: {
+                test1: {
+                    score: 0,
+                    subtests: {
+                        subtest1: { score: 1 },
+                        subtest2: { score: 0 },
+                        subtest3: { score: 0 }
+                    }
+                }
+            }
         }
-      }
-    }
 
-    const score = score_run(run, run)
-    assert.equal(score, 333) 
-  })
-  it('calculates scores based only on tests in new runs', () => {
-    const old_run = {
-      test_scores: {
-        test1: { score: 1, subtests: {} },
-        test3: { score: 0, subtests: {} }
-      }
-    }
-    const new_run = {
-      test_scores: {
-        test2: { score: 1, subtests: {} },
-        test3: { score: 1, subtests: {} }
-      }
-    }
+        const score = score_run(run, run)
+        assert.equal(score, 333)
+    })
+    it('calculates scores based only on tests in new runs', () => {
+        const old_run = {
+            test_scores: {
+                test1: { score: 1, subtests: {} },
+                test3: { score: 0, subtests: {} }
+            }
+        }
+        const new_run = {
+            test_scores: {
+                test2: { score: 1, subtests: {} },
+                test3: { score: 1, subtests: {} }
+            }
+        }
 
-    let score = score_run(old_run, new_run)
-    assert.equal(score, 0) 
+        let score = score_run(old_run, new_run)
+        assert.equal(score, 0)
 
-    old_run.test_scores.test3.score = 1
-    score = score_run(old_run, new_run) 
-    assert.equal(score, 500) 
-  })
-});
+        old_run.test_scores.test3.score = 1
+        score = score_run(old_run, new_run)
+        assert.equal(score, 500)
+    })
+})
