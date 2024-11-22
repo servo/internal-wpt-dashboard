830c56447ee855f8bd44859c1a87da01c446b95f
Mukilan Thiyagarajan <mukilan@igalia.com>
2024-11-22 09:38:53 +0530

    drop support for tracking legacy layout
    After [servo/servo#34290][1], our nightly builds no longer support
    legacy layout. Continuing support for legacy layout in the dashboard
    would require a custom build of servo. We considered displaying the
    existing 'legacy layout' scores (the line in the chart would end earlier
    than non-legacy layout), but that would require more changes in the code
    and still won't help with the table of latest scores below the chart.
    Hence, since the [plan][2] was to sunset legacy layout anyway, this
    change drops support for tracking legacy layout pass rates.
    
    [1]: https://github.com/servo/servo/pull/34290
    [2]: https://github.com/servo/project/blob/main/governance/tsc/tsc-2023-09-07.md#plan-regarding-legacy-layout-engine

--- site/load-chart.js
+++ site/load-chart.js
@@ -105,8 +105,8 @@ function setupChart () {
     const node = document.getElementById('servo-chart')
     const area_dropdown = document.getElementById('selected-area')
     const period_dropdown = document.getElementById('selected-period')
-    const show_legacy = document.getElementById('show-legacy')
     const chart = new google.visualization.LineChart(node)
+    const AREA_SCORE_OFFSET = 3
     let all_scores
 
     Object.keys(periodRanges).forEach(date => {
@@ -122,8 +122,6 @@ function setupChart () {
         const chosen_period = period_dropdown.value
         const area_index = all_scores.area_keys.indexOf(chosen_area)
         const table = new google.visualization.DataTable()
-        const stride = all_scores.area_keys.length
-        const legacy_layout = show_legacy.checked
         options.series = []
         const monthsToSubtract = periodRanges[chosen_period]
         const minDate = monthsToSubtract
@@ -132,44 +130,22 @@ function setupChart () {
 
         table.addColumn('date', 'runOn')
 
-        if (legacy_layout) {
-            options.series.push({ color: '#DC3912' })
-            table.addColumn('number', 'Legacy Layout')
-            table.addColumn({ type: 'string', role: 'tooltip', p: { html: true } })
-        }
-
         options.series.push({ color: '#3366CC' })
         table.addColumn('number', 'Servo Layout')
         table.addColumn({ type: 'string', role: 'tooltip', p: { html: true } })
 
-        for (const s of all_scores.scores) {
-            const score_2013 = s[area_index + 3]
-            const score_2020 = s[stride + area_index + 5]
-            const date = parseDateString(s[0])
+        for (const scores_for_run of all_scores.scores) {
+            const area_score = scores_for_run[area_index + AREA_SCORE_OFFSET]
+            const [date_string, wpt_sha, browser_version] = scores_for_run
+            const date = parseDateString(date_string)
             if (date < minDate) {
                 continue
             }
             const row = [
-                date
+                date,
+                area_score / 1000,
+                toolTip(date, wpt_sha, browser_version, area_score, 'Servo Layout')
             ]
-
-            if (legacy_layout) {
-                row.push(
-                    score_2013 / 1000,
-                    toolTip(date, s[1], s[2], score_2013, 'Legacy Layout')
-                )
-            }
-
-            if (score_2020 !== undefined) {
-                const wpt_sha = s[stride + 3]
-                const version = s[stride + 4]
-                row.push(
-                    score_2020 / 1000,
-                    toolTip(date, wpt_sha, version, score_2020, 'Servo Layout')
-                )
-            } else {
-                row.push(undefined, undefined)
-            }
             table.addRow(row)
         }
         chart.draw(table, options)
@@ -184,7 +160,6 @@ function setupChart () {
 
     function update_table (scores) {
         const score_table = document.getElementById('score-table-body')
-        const legacy = (value) => show_legacy.checked ? value : ''
         removeChildren(score_table)
 
         removeChildren(document.getElementById('score-table-header'))
@@ -192,20 +167,17 @@ function setupChart () {
                 'beforeend',
                 `<tr>
                     <th>Test Suite</th>
-                    ${legacy('<th>Legacy Layout</th>')}
                     <th>Servo Layout</th>
                 </tr>`
             )
 
         for (const [idx, area] of scores.area_keys.entries()) {
             const recent_score = scores.scores[scores.scores.length - 1]
-            const stride = scores.area_keys.length
             score_table.insertAdjacentHTML(
                 'beforeend',
                 `<tr class="${idx % 2 ? 'odd' : 'even'}">
                     <td>${scores.focus_areas[area]}</td>
-                    ${legacy(`<td class="score">${String(recent_score[idx + 3] / 10).padEnd(4, '.0')}%</td>`)}
-                    <td class="score">${String(recent_score[stride + idx + 5] / 10).padEnd(4, '.0')}%</td>
+                    <td class="score">${String(recent_score[idx + AREA_SCORE_OFFSET] / 10).padEnd(4, '.0')}%</td>
                 </tr>`
             )
         }
@@ -235,7 +207,6 @@ function setupChart () {
 
             area_dropdown.onchange = update
             period_dropdown.onchange = update
-            show_legacy.onchange = update
             area_dropdown.value = scores.area_keys[1]
             period_dropdown.value = Object.keys(periodRanges)[4]
             update()
