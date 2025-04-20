ac9203de285e88e5d0d17a5f6a22e85ff77008be
Oriol Brufau <obrufau@igalia.com>
2025-04-20 03:54:20 +0200

    Revert "Simplify FOCUS_AREAS structure"
    This reverts commit 619cefb705d5e054adb8cb932d8c93450ad40414.

--- process-wpt-results.js
+++ process-wpt-results.js
@@ -65,9 +65,9 @@ export function focus_areas_map (run) {
     const map = {}
     for (const test of Object.keys(run.test_scores)) {
         map[test] = []
-        for (const [index, area] of FOCUS_AREAS.entries()) {
+        for (const [area_key, area] of Object.entries(FOCUS_AREAS)) {
             if (area.predicate(test)) {
-                map[test].push(index)
+                map[test].push(area_key)
             }
         }
     }
@@ -91,91 +91,111 @@ const CSS2_FOCUS_FOLDERS = [
     'margin-padding-clear',
     'normal-flow',
     'positioning'
-];
+]
 
-const CSS_TABLES_PREDICATE = /^\/css\/(CSS2\/tables|css-tables)\//;
+const CSS_TABLES_PREDICATE = /^\/css\/(CSS2\/tables|css-tables)\//
 
-const FOCUS_AREAS = [
-    {
+const FOCUS_AREAS = {
+    all: {
         name: 'All WPT tests',
         predicate: prefix_predicate(''),
+        order: 0
     },
-    {
+    csp: {
         name: '/content-security-policy',
         predicate: prefix_predicate('/content-security-policy/'),
+        order: 1
     },
-    {
+    css: {
         name: '/css',
         predicate: prefix_predicate('/css/'),
+        order: 2
     },
-    {
+    css2: {
         name: '/css/CSS2',
         predicate: prefix_predicate('/css/CSS2/'),
+        order: 3
     },
-    ...CSS2_FOCUS_FOLDERS.map(folder => {
-        const path = `/css/CSS2/${folder}/`
-        return {
-            name: `${path}`,
-            predicate: prefix_predicate(path),
-        };
-    }),
-    {
+    csstable: {
         name: '/css/CSS2/tables & /css/css-tables',
         predicate: regex_predicate(CSS_TABLES_PREDICATE),
+        order: 90
     },
-    {
+    cssom: {
         name: '/css/cssom',
         predicate: prefix_predicate('/css/cssom/'),
+        order: 91
     },
-    {
+    cssalign: {
         name: '/css/css-align',
         predicate: prefix_predicate('/css/css-align/'),
+        order: 92
     },
-    {
+    cssflex: {
         name: '/css/css-flexbox',
         predicate: prefix_predicate('/css/css-flexbox/'),
+        order: 93
     },
-    {
+    cssgrid: {
         name: '/css/css-grid',
         predicate: prefix_predicate('/css/css-grid/'),
+        order: 94
     },
-    {
+    csspos: {
         name: '/css/css-position',
         predicate: prefix_predicate('/css/css-position/'),
+        order: 95
     },
-    {
+    csssizing: {
         name: '/css/css-sizing',
         predicate: prefix_predicate('/css/css-sizing/'),
+        order: 95.5
     },
-    {
+    csstext: {
         name: '/css/css-text',
         predicate: prefix_predicate('/css/css-text/'),
+        order: 96
     },
-    {
+    gamepad: {
         name: '/gamepad',
         predicate: prefix_predicate('/gamepad/'),
+        order: 97
     },
-    {
+    shadowdom: {
         name: '/shadow-dom',
         predicate: prefix_predicate('/shadow-dom/'),
+        order: 98
     },
-    {
+    streams: {
         name: '/streams',
         predicate: prefix_predicate('/streams/'),
+        order: 99
     },
-    {
+    trustedtypes: {
         name: '/trusted-types',
         predicate: prefix_predicate('/trusted-types/'),
+        order: 100
     },
-    {
+    webcryptoapi: {
         name: '/WebCryptoAPI',
         predicate: prefix_predicate('/WebCryptoAPI/'),
+        order: 101
     },
-    {
+    webxr: {
         name: '/webxr',
         predicate: prefix_predicate('/webxr/'),
+        order: 102
     }
-];
+}
+
+for (const [idx, folder] of CSS2_FOCUS_FOLDERS.entries()) {
+    const path = `/css/CSS2/${folder}/`
+    FOCUS_AREAS[folder] = {
+        name: `${path}`,
+        predicate: prefix_predicate(path),
+        order: idx + 3
+    }
+}
 
 export function get_focus_areas () {
     const area_keys = []
@@ -190,20 +210,23 @@ export function get_focus_areas () {
 }
 
 export function score_run (run, against_run, focus_areas_map) {
-    const scores = FOCUS_AREAS.map(() => ({
-        total_tests: 0,
-        total_score: 0,
-        total_subtests: 0,
-        total_subtests_passed: 0
-    }));
+    const scores = {}
+    for (const area of Object.keys(FOCUS_AREAS)) {
+        scores[area] = {
+            total_tests: 0,
+            total_score: 0,
+            total_subtests: 0,
+            total_subtests_passed: 0
+        }
+    }
 
     for (const [test, { subtests }] of Object.entries(against_run.test_scores)) {
-        const area_indices = focus_areas_map[test]
+        const areas = focus_areas_map[test]
         const subtest_names = Object.keys(subtests)
 
-        for (const index of area_indices) {
-            scores[index].total_tests += 1
-            scores[index].total_subtests += !subtest_names.length ? 1 : subtest_names.length
+        for (const area of areas) {
+            scores[area].total_tests += 1
+            scores[area].total_subtests += !subtest_names.length ? 1 : subtest_names.length
         }
 
         const run_test = run.test_scores[test]
@@ -212,9 +235,9 @@ export function score_run (run, against_run, focus_areas_map) {
         if (!run_test) continue
 
         if (!subtest_names.length) {
-            for (const index of area_indices) {
-                scores[index].total_score += run_test.score
-                scores[index].total_subtests_passed += run_test.score
+            for (const area of areas) {
+                scores[area].total_score += run_test.score
+                scores[area].total_subtests_passed += run_test.score
             }
         } else {
             let subtests_passed = 0
@@ -223,9 +246,9 @@ export function score_run (run, against_run, focus_areas_map) {
                     subtests_passed += run_test.subtests[subtest].score
                 }
             }
-            for (const index of area_indices) {
-                scores[index].total_score += subtests_passed / subtest_names.length
-                scores[index].total_subtests_passed += subtests_passed
+            for (const area of areas) {
+                scores[area].total_score += subtests_passed / subtest_names.length
+                scores[area].total_subtests_passed += subtests_passed
             }
         }
     }
